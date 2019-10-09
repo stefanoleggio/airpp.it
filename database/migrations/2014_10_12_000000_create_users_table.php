75961864d3de26bed9c34048c9fbbd632672359d
Stefano Leggio <stefanoleggio28@gmail.com>
2019-10-09 17:09:25 +0200

    login

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,7 +22,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email',191)->unique();
             $table->string('password');
             $table->string('role');
             $table->datetime('last_login_at')->nullable();
