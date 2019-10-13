7ae46f127cb654648757547af89236f7640e5beb
Stefano Leggio <stefanoleggio28@gmail.com>
2019-10-13 13:26:25 +0200

    adj

--- resources/views/templates/admin.blade.php
+++ resources/views/templates/admin.blade.php
@@ -5,7 +5,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <title>{{ config('app.name', 'Laravel') }}</title>
-  <link rel="stylesheet" href="{{ asset('/css/modal.min.css') }}"/>
   <link href="{{ asset('css/admin.css') }}" rel="stylesheet">
   <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">
   <script src="{{ asset('jquery/jquery.js') }}"></script>
