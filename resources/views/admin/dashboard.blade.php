7e9f14700e89dfcce347b88f11c5efc8f477f7d6
Stefano Leggio <stefanoleggio28@gmail.com>
2020-04-08 16:52:20 +0200

    Dasboard update

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -7,73 +7,23 @@
             'description' => 'Benvenuto nel pannello amministrativo di airpp.it'
         ]
     )
-    <script>
-      (function(w,d,s,g,js,fs){
-        g=w.gapi||(w.gapi={});g.analytics={q:[],ready:function(f){this.q.push(f);}};
-        js=d.createElement(s);fs=d.getElementsByTagName(s)[0];
-        js.src='https://apis.google.com/js/platform.js';
-        fs.parentNode.insertBefore(js,fs);js.onload=function(){g.load('analytics');};
-      }(window,document,'script'));
-    </script>
-    <div id="embed-api-auth-container"></div>
-    <div id="chart-container"></div>
-    <div id="view-selector-container"></div>
-    <script>
+    <div class="container">
+      <div class="text-center">
+        <iframe width="600" height="371" class="embed-responsive-item" seamless frameborder="0" scrolling="no" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQOPTfMi_OMgWF-mh9mvyx9kmBdWwU5GVpuTgCfWOfDzQG7lZc18L5iSmcV0N9-sAjRBS58Xx8PtLNQ/pubchart?oid=1194469025&amp;format=interactive"></iframe>
+      </div>
+      <div class="text-center mt-5">
+        <iframe width="600" height="371" seamless frameborder="0" scrolling="no" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQOPTfMi_OMgWF-mh9mvyx9kmBdWwU5GVpuTgCfWOfDzQG7lZc18L5iSmcV0N9-sAjRBS58Xx8PtLNQ/pubchart?oid=1072739423&amp;format=interactive"></iframe>
+          </div>
+      <div class="text-center mt-5">
+        <iframe width="600" height="371" class="embed-responsive-item" seamless frameborder="0" scrolling="no" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQOPTfMi_OMgWF-mh9mvyx9kmBdWwU5GVpuTgCfWOfDzQG7lZc18L5iSmcV0N9-sAjRBS58Xx8PtLNQ/pubchart?oid=1742141464&amp;format=interactive"></iframe>
+      </div>
+      <div class="text-center mt-5">
+        <iframe width="600" height="371" seamless frameborder="0" scrolling="no" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQOPTfMi_OMgWF-mh9mvyx9kmBdWwU5GVpuTgCfWOfDzQG7lZc18L5iSmcV0N9-sAjRBS58Xx8PtLNQ/pubchart?oid=66749362&amp;format=interactive"></iframe>
+      </div>
+      <div class="text-center mt-5">
+      <iframe width="600" height="371" seamless frameborder="0" scrolling="no" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQOPTfMi_OMgWF-mh9mvyx9kmBdWwU5GVpuTgCfWOfDzQG7lZc18L5iSmcV0N9-sAjRBS58Xx8PtLNQ/pubchart?oid=37859&amp;format=interactive"></iframe>
+        </div>
 
-      gapi.analytics.ready(function() {
-
-        /**
-        * Authorize the user immediately if the user has already granted access.
-        * If no access has been created, render an authorize button inside the
-        * element with the ID "embed-api-auth-container".
-        */
-        gapi.analytics.auth.authorize({
-          container: 'embed-api-auth-container',
-          clientid: "{{env('ANALYTICS_ID')}}"
-        });
-
-
-        /**
-        * Create a new ViewSelector instance to be rendered inside of an
-        * element with the id "view-selector-container".
-        */
-        var viewSelector = new gapi.analytics.ViewSelector({
-          container: 'view-selector-container'
-        });
-
-        // Render the view selector to the page.
-        viewSelector.execute();
-
-
-        /**
-        * Create a new DataChart instance with the given query parameters
-        * and Google chart options. It will be rendered inside an element
-        * with the id "chart-container".
-        */
-        var dataChart = new gapi.analytics.googleCharts.DataChart({
-          query: {
-            metrics: 'ga:sessions',
-            dimensions: 'ga:date',
-            'start-date': '30daysAgo',
-            'end-date': 'yesterday'
-          },
-          chart: {
-            container: 'chart-container',
-            type: 'LINE',
-            options: {
-              width: '100%'
-            }
-          }
-        });
-
-
-        /**
-        * Render the dataChart on the page whenever a new view is selected.
-        */
-        viewSelector.on('change', function(ids) {
-          dataChart.set({query: {ids: ids}}).execute();
-        });
-
-      });
-      </script>
+    </div>
+    
 @endsection
