7eca860c06bcaf1ddcaf095d4b89ceb07db20708
stefano28 <stefanoleggio28@gmail.com>
2021-01-14 19:39:22 +0100

    hotjar

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -40,3 +40,14 @@
 
   gtag('config', "{{env('ANALYTICS_ID')}}");
 </script>
+<!-- Hotjar Tracking Code for www.airpp.it -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:2197201,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
