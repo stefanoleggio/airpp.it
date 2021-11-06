715cf775b856f72226318230289f02e06512a5bb
stefano28 <stefanoleggio28@gmail.com>
2021-11-06 12:12:37 +0100

    IE Alert

--- resources/views/templates/page.blade.php
+++ resources/views/templates/page.blade.php
@@ -6,16 +6,40 @@
         @include('includes.aos')
         @include('cookieConsent::index')
         @include('includes.topbar')
+        <!-- IE Alert -->
+
+            <div id="IEAlert" class="modal fade">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                            <h4 class="modal-title">Attenzione, browser non supportato</h4>
+                        </div>
+                        <div class="modal-body">
+                        <div class="modal-pap">
+                            <h2>
+                            Internet Explorer non consente la visualizzazione corretta di alcuni elementi del sito web.
+                            </h2>
+                            Si prega di usare un browser differente come Mozilla Firefox, Google Chrome, Opera, Safari, Microsoft Edge.
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <script>
+            if (window.document.documentMode) {
+                setTimeout(function() {
+                    $('#IEAlert').modal('show');
+                }, 4000);
+            }
+        </script>
+        <!-- -->
         @if (Request::path() != '/')
             <div class="topbar-offset">
             </div>
             @yield('content')
         @endif
         @if(Request::path() == '/')
-            <!--[if IE]>
-            Non-IE browsers ignore this
-            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
-            <![endif]-->
             <div id="content">
                 @yield('content')
             </div>
